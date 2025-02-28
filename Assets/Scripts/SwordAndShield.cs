a294f0a9d38125fbd0f6fc3a4d849465d8cd6a58
BLarios11 <brianlarios11@gmail.com>
2025-02-27 22:31:05 -0800

    Push

---
+++ Assets/Scripts/SwordAndShield.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SwordAndShield : Weapons
+{
+    
+    public SwordAndShield()
+    {
+        damage = 10;
+        weaponName = "Sword and Shield";
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public int damage;
+    public string weaponName;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
